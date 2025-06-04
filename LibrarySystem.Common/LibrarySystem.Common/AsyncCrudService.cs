b66e8fdf794c937809401ee40b8c48f0b5504a37
Artem Nalyvaiko <artt@email.com>
2025-06-04 22:22:10 +0300

    Lab 2 done

---
+++ LibrarySystem.Common/LibrarySystem.Common/AsyncCrudService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LibrarySystem.Common
+{
+    public class AsyncCrudService<T> : ICrudServiceAsync<T> where T : class
+    {
+        private readonly ConcurrentDictionary<Guid, T> _storage = new();
+        private readonly Func<T, Guid> _getId;
+        private readonly SemaphoreSlim _fileSemaphore = new(1, 1); // для сохранения файла
+        private readonly object _lock = new(); // для защиты операций с коллекцией
+
+        public AsyncCrudService(Func<T, Guid> getId)
+        {
+            _getId = getId;
+        }
+
+        public async Task<bool> CreateAsync(T element)
+        {
+            var id = _getId(element);
+            lock (_lock)
+            {
+                return _storage.TryAdd(id, element);
+            }
+        }
+
+        public async Task<T> ReadAsync(Guid id)
+        {
+            _storage.TryGetValue(id, out var value);
+            return value;
+        }
+
+        public async Task<IEnumerable<T>> ReadAllAsync()
+        {
+            return _storage.Values.ToList();
+        }
+
+        public async Task<IEnumerable<T>> ReadAllAsync(int page, int amount)
+        {
+            return _storage.Values
+                .Skip((page - 1) * amount)
+                .Take(amount)
+                .ToList();
+        }
+
+        public async Task<bool> UpdateAsync(T element)
+        {
+            var id = _getId(element);
+            lock (_lock)
+            {
+                _storage[id] = element;
+                return true;
+            }
+        }
+
+        public async Task<bool> RemoveAsync(T element)
+        {
+            var id = _getId(element);
+            lock (_lock)
+            {
+                return _storage.TryRemove(id, out _);
+            }
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            await _fileSemaphore.WaitAsync();
+            try
+            {
+                var json = JsonSerializer.Serialize(_storage.Values, new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                });
+                await File.WriteAllTextAsync("storage.json", json);
+                return true;
+            }
+            finally
+            {
+                _fileSemaphore.Release();
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator() => _storage.Values.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
--- LibrarySystem.Common/LibrarySystem.Common/Book.cs
+++ LibrarySystem.Common/LibrarySystem.Common/Book.cs
@@ -9,6 +9,12 @@ namespace LibrarySystem.Common
         public int Год { get; set; }
         public string Автор { get; set; }
 
+        // Пустой конструктор для сериализации и Parallel.For
+        public Book()
+        {
+            Id = Guid.NewGuid();
+        }
+
         public Book(string название, int год, string автор)
         {
             Id = Guid.NewGuid();
@@ -20,6 +26,21 @@ namespace LibrarySystem.Common
         public override string ToString()
         {
             return $"Книга: {Название}, Автор: {Автор}, Год: {Год}";
+        }
+
+        // 🔧 Метод для генерации случайной книги (нужно для ЛР №2)
+        public static Book CreateNew()
+        {
+            var random = new Random();
+            var authors = new[] { "Тарас Шевченко", "Ліна Костенко", "Лев Толстой", "Франс Кафка", "Дж. Р. Р. Толкін" };
+            var titles = new[] { "Таємниці лісу", "Космічні пригоди", "Історія душі", "Втеча в безодню", "Книга ночі" };
+
+            return new Book
+            {
+                Название = titles[random.Next(titles.Length)],
+                Автор = authors[random.Next(authors.Length)],
+                Год = random.Next(1900, 2025)
+            };
         }
     }
 }
